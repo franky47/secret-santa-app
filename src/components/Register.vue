f08d8122abee558b1cfbb898f19e883ceb55a7d0
Francois Best <francoisbest@gmail.com>
2016-08-19 02:41:31 +0200

    Connecting registering logic.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent='register' class='ui form'>
+    <form @submit.prevent='register' class='ui form' :class='{loading: loading, error: errorMessage}'>
         <div class='field' :class='{error: check(validation.name)}'>
             <label>Name <span v-if='check(validation.name)'>cannot be empty</span></label>
             <input type='text' v-model='newUser.name' placeholder='Santa Claus'>
@@ -12,6 +12,10 @@
             <label>Password (8 characters minimum)</label>
             <input v-model='newUser.password' type='password' placeholder='8 characters minimum'>
         </div>
+        <div class='ui error message'>
+            <div class='header'>Error</div>
+            <p>{{errorMessage}}</p>
+        </div>
         <button class='ui basic green button' type='submit'>
             <i class='fitted icons'>
                 <i class='icon user'></i>
@@ -22,17 +26,22 @@
 </template>
 
 <script>
-import { registerWithEmail } from '../vuex/modules/auth/actions'
-const emailRE = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+import {
+    registerWithEmail,
+    updateUserProfile
+} from '../vuex/modules/auth/actions'
+const emailRE = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export default {
     data: () => ({
         displayErrors: false,
+        loading: false,
         newUser: {
             name: '',
             email: '',
             password: ''
-        }
+        },
+        errorMessage: ''
     }),
     computed: {
         validation: function() {
@@ -49,22 +58,39 @@ export default {
     },
     methods: {
         register() {
-            if (this.isValid) {
-                this.registerWithEmail(this.newUser.email,
-                                       this.newUser.password)
-                this.newUser.email = ''
-                this.newUser.password = ''
-            } else {
+            if (!this.isValid) {
                 this.displayErrors = true
+                return
             }
+            this.registerWithEmail(this.newUser.email, this.newUser.password)
+                .then(() => {
+                    console.log('Created user, updating profile')
+                    const profile = { displayName: this.newUser.name }
+                    return this.updateUserProfile(profile)
+                }).then(() => { // success
+                    this.loading = false
+                    this.reset()
+                }).catch((error) => { // failed
+                    this.loading = false
+                    this.errorMessage = error.message
+                })
+            this.loading = true
         },
         check(field) {
             return this.displayErrors && !field
+        },
+        reset() {
+            this.displayErrors = false
+            this.newUser.email = ''
+            this.newUser.password = ''
+            this.newUser.name = ''
+            this.errorMessage = ''
         }
     },
     vuex: {
         actions: {
-            registerWithEmail
+            registerWithEmail,
+            updateUserProfile
         }
     }
 }
