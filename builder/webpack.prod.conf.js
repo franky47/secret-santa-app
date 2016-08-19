501162c77c52dbed741bf08120ec148e3d7441e6
Francois Best <francoisbest@gmail.com>
2016-08-19 23:58:58 +0200

    build/ replaces dist/ as output dir.

--- builder/webpack.prod.conf.js
+++ builder/webpack.prod.conf.js
@@ -39,7 +39,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.OccurenceOrderPlugin(),
     // extract css into its own file
     new ExtractTextPlugin(utils.assetsPath('css/[name].[contenthash].css')),
-    // generate dist index.html with correct asset hash for caching.
+    // generate build/index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
