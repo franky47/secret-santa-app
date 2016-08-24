7e71cb9ac2f09288a93344448b1d6e2e3add028f
Francois Best <francoisbest@gmail.com>
2016-08-25 01:40:23 +0200

    Profile sub-pages stubs.

---
+++ src/views/profile/Account.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Manage your account</h2>
+    <p>Change your email</p>
+    <p>Change your password</p>
+    <p>Delete your account</p>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    })
+}
+</script>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Your profile</h2>
+    <p>Change your name</p>
+    <p>Change your avatar</p>
+    <p>Other app-specific stuff here...</p>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    })
+}
+</script>
