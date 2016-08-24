e55451df19c191eb4c82fa858b4b1f03bb144c3c
Francois Best <francoisbest@gmail.com>
2016-08-24 09:43:56 +0200

    Moved register & sign-in logic to own views.

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,15 +1,129 @@
 <template>
     <div class='wrapper'>
         <h1>Create New Account</h1>
-        <register></register>
+        <form @submit.prevent='register' class='ui form' :class='{error: errorMessage}'>
+            <email :value.sync='user.email' :show-errors='showErrors' v-ref:email required></email>
+            <password :value.sync='user.password' :show-errors='showErrors' v-ref:password instructions required></password>
+            <name :value.sync='user.name' :show-errors='showErrors' v-ref:name required></name>
+            <div class='field' v-if='user.name'
+                style='user-select:none' unselectable='on'
+                onselectstart='return false'
+                onmousedown='return false'
+            >
+                <label>Avatar <i class='ui small grey icon refresh' @click='changeColor'></i></label>
+                <avatar :back-color='user.avatarColor' :text='avatarLetter'></avatar>
+            </div>
+            <div class='ui error message'>
+                <div class='header'>Error</div>
+                <p>{{errorMessage}}</p>
+            </div>
+            <button class='ui basic green button' :class='{loading: loading}' type='submit'>
+                <i class='fitted icons'>
+                    <i class='icon user'></i>
+                    <i class='corner add icon'></i>
+                </i> Create Account
+            </button>
+        </form>
     </div>
 </template>
 
 <script>
-import Register from '../../components/Register'
+import Avatar   from '../../components/Avatar'
+import Email    from '../../components/form/Email'
+import Password from '../../components/form/Password'
+import Name     from '../../components/form/Name'
+import materialColors from '../../utility/material-colors'
+import {
+    registerWithEmail,
+    updateUserProfile
+} from '../../vuex/modules/auth/actions'
+
 export default {
+    data: () => ({
+        showErrors: false,
+        loading: false,
+        user: {
+            email: '',
+            password: '',
+            name: '',
+            avatarColor: ''
+        },
+        errorMessage: ''
+    }),
     components: {
-        Register
+        Email,
+        Password,
+        Name,
+        Avatar
+    },
+    computed: {
+        validation() {
+            const email     = this.$refs.email    || { valid: false }
+            const password  = this.$refs.password || { valid: false }
+            const name      = this.$refs.name     || { valid: false }
+            return {
+                name:       name.valid,
+                email:      email.valid,
+                password:   password.valid
+            }
+        },
+        isValid() {
+            const validation = this.validation
+            return Object.keys(validation).every((key) => validation[key])
+        },
+        avatarLetter() {
+            return (this.user.name.trim()[0] || '').toUpperCase()
+        }
+    },
+    methods: {
+        register() {
+            if (!this.isValid) {
+                this.showErrors = true
+                return
+            }
+            this.registerWithEmail(this.user.email, this.user.password)
+                .then(() => {
+                    const avatar = this.$children[0]
+                    const photoURL = avatar.convertToInlineImage()
+                    const profile = {
+                        displayName: this.user.name,
+                        photoURL
+                    }
+                    return this.updateUserProfile(profile)
+                }).then(() => { // success
+                    this.loading = false
+                    this.reset()
+                }).catch((error) => { // failed
+                    this.loading = false
+                    this.errorMessage = error.message
+                })
+            this.loading = true
+        },
+        check(field) {
+            return this.showErrors && !field
+        },
+        reset() {
+            this.showErrors = false
+            this.user.email = ''
+            this.user.password = ''
+            this.user.name = ''
+            this.errorMessage = ''
+        },
+        changeColor() {
+            const colors = []
+            for (let color in materialColors) { colors.push(materialColors[color]) }
+            const pick = (array) => array[Math.floor(Math.random() * array.length)]
+            this.user.avatarColor = pick(colors)
+        }
+    },
+    vuex: {
+        actions: {
+            registerWithEmail,
+            updateUserProfile
+        }
+    },
+    attached() {
+        this.changeColor()
     }
 }
 </script>
--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -1,21 +1,106 @@
 <template>
-    <!-- todo: check fragmentation error here -->
-    <sign-in class='wrapper'></sign-in>
+    <div id='wrapper' class='ui very padded raised segment' :class='{loading: loading}'>
+        <button @click='signInWithFacebook' class='ui fluid large facebook button'>
+            <i class='facebook icon'></i>
+            Sign in with Facebook
+        </button>
+        <div class='ui horizontal divider'>Or</div>
+        <form @submit.prevent='signIn' class='ui form' :class='{error: errorMessage}'>
+            <email :value.sync='user.email' :show-errors='showErrors' v-ref:email></email>
+            <password :value.sync='user.password' :show-errors='showErrors' v-ref:password></password>
+            <button class='ui fluid blue button' type='submit'>
+                <i class='fitted icon sign in'></i> Sign In
+            </button>
+            <div class='ui error message'>
+                <div class='header'>Error</div>
+                <p>{{errorMessage}}</p>
+            </div>
+        </form>
+        <div class='ui divider'></div>
+        <div class='register'>
+            <a href='#'>Create an account</a></p>
+        </div>
+    </div>
 </template>
 
 <script>
-import SignIn from '../../components/SignIn'
+import Email from '../../components/form/Email'
+import Password from '../../components/form/Password'
+import { isSignedIn } from '../../vuex/modules/auth/getters'
+import {
+    signInWithFacebook,
+    signInWithEmail,
+    signOut
+} from '../../vuex/modules/auth/actions'
+import * as routes from '../../router/routes-definitions'
+
 export default {
-    components: {
-        SignIn
+    data: () => ({
+        user: {
+            email: '',
+            password: ''
+        },
+        loading: false,
+        errorMessage: ''
+    }),
+    components: { Email, Password },
+    methods: {
+        signIn() {
+            const email = this.user.email.trim()
+            const password = this.user.password.trim()
+            // todo: check route args to see if reauth or sign-in.
+            this.signInWithEmail(email, password).then(() => {
+                this.reset()
+                const redirect = this.redirect
+                if (redirect) {
+                    this.$router.go({ path: redirect })
+                } else {
+                    this.$router.go(routes.home)
+                }
+            }).catch(error => {
+                this.loading = false
+                this.errorMessage = error.message
+            })
+            this.loading = true
+        },
+        reset() {
+            this.errorMessage = ''
+            this.user.email = ''
+            this.user.password = ''
+            this.loading = false
+        }
+    },
+    watch: {
+        isSignedIn(signedIn) {
+            const redirect = this.redirect
+            if (signedIn && redirect) {
+                this.$router.go({ path: redirect })
+            }
+        }
+    },
+    vuex: {
+        getters: {
+            isSignedIn,
+            redirect: state => state.route.query.then || null
+        },
+        actions: {
+            signInWithFacebook,
+            signInWithEmail,
+            signOut
+        }
     }
 }
 </script>
 
 <style scoped>
-.wrapper {
+#wrapper {
     max-width: 450px;
     min-width: 360px;
     margin: 130px auto;
+    padding-bottom: 23px;
+    background: #fafafa;
+}
+.register {
+    text-align: center;
 }
 </style>
