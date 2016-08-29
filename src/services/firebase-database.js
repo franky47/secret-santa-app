74ecd4adc48cb1f9416a8c984f6eadc576d03930
Francois Best <francoisbest@gmail.com>
2016-08-29 09:40:49 +0200

    Firebase db helpers.

--- src/services/firebase-database.js
+++ src/services/firebase-database.js
@@ -6,4 +6,20 @@ export class FirebaseDatabaseService extends FirebaseFeature {
         super(firebaseApp, 'db')
         this.db = firebase.database(this.app)
     }
+
+    // Write --
+    set(path, data) {
+        return this.db.ref(path).set(data)
+    }
+    push(path, data) {
+        return this.db.ref(path).push(data)
+    }
+    update(path, data) {
+        return this.db.ref(path).update(data)
+    }
+
+    // Read --
+    once(path) {
+        return this.db.ref(path).once('value')
+    }
 }
