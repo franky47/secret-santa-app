abce12092880065569b1b2fd0d43910d48af33ef
Francois Best <francoisbest@gmail.com>
2016-08-31 20:19:39 +0200

    Regexping locale loading with fallback to English.

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,19 +1,27 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-// Use l10n if needs grow bigger
 export const localeNames = {
     'en': 'English',
     'fr': 'Français'
 }
 
-export const supportedLocales = [
-    'en', 'fr'
-]
+export const supportedLocales = {
+    'en': /en[-_]?.*/,
+    'fr': /fr[-_]?.*/
+}
 
 export const loadLocale = locale => {
     return new Promise((resolve, reject) => {
         try {
+            let matched = null
+            for (const key in supportedLocales) {
+                if (locale.match(supportedLocales[key])) {
+                    matched = key
+                    break
+                }
+            }
+            locale = matched === null ? 'en' : locale
             Vue.locale(locale, require(`./translations/${locale}`).default)
             resolve(locale)
         } catch (error) {
