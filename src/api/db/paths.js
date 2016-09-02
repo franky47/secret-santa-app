37a1be19b9e8938dcd87aa87caeb5ed44daca947
Francois Best <francoisbest@gmail.com>
2016-09-02 17:31:05 +0200

    Moved database paths to own definitions file.

---
+++ src/api/db/paths.js
@@ -0,0 +1,3 @@
+export const users  = '/users'
+export const user   = (uid) => `/users/${uid}`
+
--- src/api/db/user.js
+++ src/api/db/user.js
@@ -1,5 +1,6 @@
 import firebase from '../../services/firebase'
 import { errorWhile } from '../../utility/utility'
+import * as paths from './paths'
 
 export const mock = {
     users: {
@@ -12,7 +13,7 @@ export const mock = {
 }
 
 export const createUser = (uid, user = {}) => {
-    const path = `/users/${uid}`
+    const path = paths.user(uid)
     return firebase.db.once(path).then(snapshot => {
         if (snapshot.val() !== null) {
             return Promise.resolve() // Already exists
@@ -25,6 +26,10 @@ export const createUser = (uid, user = {}) => {
 }
 
 export const updateUser = (uid, user) => {
-    return firebase.db.update(`/users/${uid}`, user)
+    return firebase.db.update(paths.user(uid), user)
         .catch(errorWhile('updating user'))
+}
+
+export const watchUser = (uid, callback) => {
+    return firebase.db.onValueChanged(paths.user(uid), callback)
 }
