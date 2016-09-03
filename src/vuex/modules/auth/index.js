16376b3dfceb93f24888f0473075088fc1c4713c
Francois Best <francoisbest@gmail.com>
2016-09-03 19:25:21 +0200

    Fix exports.

--- src/vuex/modules/auth/index.js
+++ src/vuex/modules/auth/index.js
@@ -2,6 +2,4 @@ import mutations from './mutations'
 import * as getters from './getters'
 import * as actions from './actions'
 
-export const mutations
-export const getters
-export const actions
+export { mutations, getters, actions }
