bd2a19920540c10c9f7571de20dd49517d1688e4
Francois Best <francoisbest@gmail.com>
2016-09-03 09:20:27 +0200

    Settings panel UI.

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,14 +1,43 @@
 <template>
-    <div class='wrapper'>
-        <h1 class='ui header'>Settings</h1>
-        <router-view></router-view>
+    <div class='ui two column grid'>
+        <div class='four wide column'>
+            <h1 class='ui header'>Settings</h1>
+            <div class='ui vertical pointing menu'>
+                <a v-for='item in menu' class='item' v-link='item.link'>{{ item.name }}</a>
+            </div>
+            <button class='ui basic icon button'>
+                <i class='icon sign out'></i>
+                {{ $t('auth.signOut') }}
+            </button>
+        </div>
+        <div class='column'>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import * as routes from '../router/routes-definitions'
+
 export default {
     data: () => ({
-
-    })
+        links: [
+            { name: 'account', path: routes.settings.account },
+            { name: 'profile', path: routes.settings.profile }
+        ]
+    }),
+    computed: {
+        menu() {
+            return this.links.map(e => ({
+                name: Vue.t(e.name),
+                link: {
+                    path: e.path,
+                    activeClass: 'active',
+                    exact: true
+                }
+            }))
+        }
+    }
 }
 </script>
