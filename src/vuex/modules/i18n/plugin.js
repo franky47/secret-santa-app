dec593adc9612b669917e5d3bed20f5b193943cb
Francois Best <francoisbest@gmail.com>
2016-09-06 08:42:47 +0200

    Save locale per user in local storage.

--- src/vuex/modules/i18n/plugin.js
+++ src/vuex/modules/i18n/plugin.js
@@ -1,10 +1,26 @@
-import mutations from './mutations'
+import i18nMutations from './mutations'
+import * as user from '../user'
 import localStorage from '../../../api/localStorage'
 import { locale } from './getters'
 
+let userId = null
+
+const getLocaleKey = () => userId ? `locale_${userId}` : 'locale'
+
 const subscriptions = {
-    [mutations.SET_LOCALE]: ({state}) => {
-        localStorage.set('locale', locale(state))
+    [i18nMutations.SET_LOCALE]: ({state}) => {
+        localStorage.set(getLocaleKey(), locale(state))
+    },
+    [user.mutations.CHANGED]: ({state}) => {
+        const prevKey  = getLocaleKey()
+        const prevUser = userId
+        userId = user.getters.getCurrentUser.uid(state)
+        if (prevUser === null && userId !== null) {
+            // Copy "root" setting to dedicated user
+            const value = localStorage.get(prevKey)
+            localStorage.delete(prevKey)
+            localStorage.set(getLocaleKey(), value)
+        }
     }
 }
 
