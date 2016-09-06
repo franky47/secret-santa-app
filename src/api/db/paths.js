9f272c48d852c35125d5e53723ffb5aa838c73a0
Francois Best <francoisbest@gmail.com>
2016-09-06 09:48:02 +0200

    Added preferences db paths.

--- src/api/db/paths.js
+++ src/api/db/paths.js
@@ -1,2 +1,6 @@
-export const users  = '/users'
-export const user   = (uid) => `/users/${uid}`
+export const users              = '/users'
+export const user               = (uid) => `${users}/${uid}`
+export const userPreferences = {
+    root:       (uid) => `${user(uid)}/preferences`,
+    locale:     (uid) => `${user(uid)}/preferences/locale`
+}
