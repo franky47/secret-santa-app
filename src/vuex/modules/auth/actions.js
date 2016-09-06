50657070b2488768bd1117aa4d792bdb19e5351b
Francois Best <francoisbest@gmail.com>
2016-09-06 08:41:21 +0200

    Relying only on auth/USER_CHANGED for user ops.

--- src/vuex/modules/auth/actions.js
+++ src/vuex/modules/auth/actions.js
@@ -1,4 +1,5 @@
 import mutations from './mutations'
+import userMutations from '../user/mutations'
 import { isSignedIn } from './getters'
 import firebase from '../../../services/firebase'
 
@@ -11,48 +12,31 @@ const dispatchAndChain = dispatch => error => {
 
 export const registerWithEmail = ({dispatch}, email, password) => {
     return firebase.auth.registerWithEmail(email, password)
-        .then(result => {
-            dispatch(mutations.SIGNED_IN)
-            return result
-        })
         .catch(dispatchAndChain(dispatch))
 }
 export const signInWithFacebook = ({dispatch}) => {
-    // Using redirect: don't mark as signed in yet, wait for completion.
     return firebase.auth.signInWithFacebook()
         .catch(dispatchAndChain(dispatch))
 }
 export const signInWithEmail = ({dispatch}, email, password) => {
     return firebase.auth.signInWithEmail(email, password)
-        .then(result => {
-            dispatch(mutations.SIGNED_IN)
-            return result
-        })
         .catch(dispatchAndChain(dispatch))
 }
 export const signOut = ({dispatch}) => {
     return firebase.auth.signOut()
-        .then(result => {
-            dispatch(mutations.SIGNED_OUT)
-            return result
-        })
         .catch(dispatchAndChain(dispatch))
 }
 export const updateUserProfile = ({dispatch}, profile) => {
     const user = firebase.auth.currentUser
     return firebase.auth.updateUserProfile(profile)
         .then(result => {
-            dispatch(mutations.USER_CHANGED, user)
+            dispatch(userMutations.CHANGED, user)
             return result
         })
         .catch(dispatchAndChain(dispatch))
 }
 export const deleteAccount = ({dispatch}) => {
     return firebase.auth.deleteUserAccount()
-        .then(result => {
-            dispatch(mutations.SIGNED_OUT)
-            return result
-        })
         .catch(dispatchAndChain(dispatch))
 }
 
@@ -60,10 +44,13 @@ export const deleteAccount = ({dispatch}) => {
 
 export const authChangedCallback = ({dispatch, state}, user) => {
     console.log('Auth changed to', user)
-    dispatch(mutations.USER_CHANGED, user)
     if (user && !isSignedIn(state)) {
         dispatch(mutations.SIGNED_IN)
     }
+    if (!user && isSignedIn(state)) {
+        dispatch(mutations.SIGNED_OUT)
+    }
+    dispatch(mutations.USER_CHANGED, user)
 }
 export const authErrorCallback = ({dispatch}, error) => {
     console.log('Auth error:', error)
--- src/vuex/modules/user/plugin.js
+++ src/vuex/modules/user/plugin.js
@@ -6,42 +6,48 @@ import {
 import userMutations from './mutations'
 import authMutations from '../auth/mutations'
 import { getCurrentUserObject } from './getters'
-import { errorWhile } from '../../../utility'
 
-const watchers = {
+const observers = {
     user: {
-        off: () => {}
+        start(user, dispatch) {
+            if (!user || !user.uid) {
+                throw new TypeError("Can't observe null user id")
+            }
+            this.unregister() // stop observing previous user (if any)
+            createUser(user.uid, user).then(() => {
+                this.unregister = watchUser(user.uid, snapshot => {
+                    const data = snapshot.val()
+                    if (data) {
+                        dispatch(userMutations.CHANGED, data)
+                    } else {
+                        console.log('Could not find user info')
+                    }
+                })
+            })
+        },
+        stop() {
+            this.unregister()
+            this.unregister = () => {}
+        },
+        unregister: () => {}
     }
 }
 
 const subscriptions = {
     [userMutations.CHANGED]: ({state}) => {
-        console.log('user changed, sub => create/update in db')
         const user = getCurrentUserObject(state)
         createUser(user.uid, user).then(() => {
             updateUser(user.uid, user)
+        }).catch(error => {
+            console.log(`Error while creating/updating user on ${userMutations.CHANGED}: ${error.message}`)
         })
-    },
-    [authMutations.SIGNED_IN]: ({dispatch, state}) => {
-        console.log('user signed in, sub => attach listeners')
-        const user = state.auth.user
-        watchers.user.off() // Unregister previous watcher (if any)
-        watchers.user.off = watchUser(user.uid, snapshot => {
-            const user = snapshot.val()
-            dispatch(userMutations.CHANGED, user)
-        })
-    },
-    [authMutations.SIGNED_OUT]: ({dispatch}) => {
-        console.log('user signed out, sub => detach listeners')
-        watchers.user.off() // Unregister watcher
-        watchers.user.off = () => {}
-        dispatch(userMutations.RESET)
     },
     [authMutations.USER_CHANGED]: ({dispatch}, mutation) => {
         const user = mutation.payload[0]
         if (user) {
-            console.log('User changed in auth module, updating user info', user)
-            dispatch(userMutations.CHANGED, user)
+            observers.user.start(user, dispatch)
+        } else {
+            observers.user.stop()
         }
     }
 }
