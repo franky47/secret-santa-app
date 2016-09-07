d9294d60a9539df4ec0e8df87d038173328d4c24
Francois Best <francoisbest@gmail.com>
2016-09-07 11:19:04 +0200

    Added Tree observer, refactoring classes.

--- src/services/observers.js
+++ src/services/observers.js
@@ -1,17 +1,43 @@
 import firebase from './firebase'
 
-export class ValueChangedObserver {
-    constructor() {
-        this.unregister = () => {}
+class ObserverBase {
+    /**
+     * Create an Observer
+     * @param {methodResolver} - Function resolving to a Firebase database method.
+     * This is needed to let the database initialize, and will be bound & resolved
+     * when starting the observer (the database will have to be ready at that point).
+     */
+    constructor(methodResolver) {
+        this.unregister     = () => {}
+        this.methodResolver = methodResolver
+        this.firebaseMethod = null
     }
-    start(path, callback, initData = null) {
+    initialize() {
+        if (!this.firebaseMethod) {
+            this.firebaseMethod = this.methodResolver()
+        }
+    }
+    start(path, callback) {
+        this.initialize()
         this.unregister()
-        this.unregister = firebase.db.onValueChanged(path, snapshot => {
+        this.unregister = this.firebaseMethod(path, snapshot => {
             callback(snapshot.val())
         })
     }
     stop() {
         this.unregister()
         this.unregister = () => {}
+    }
+}
+
+export class ValueChangedObserver extends ObserverBase {
+    constructor() {
+        super(() => firebase.db.onValueChanged.bind(firebase.db))
+    }
+}
+
+export class TreeObserver extends ObserverBase {
+    constructor() {
+        super(() => firebase.db.onChildrenChanged.bind(firebase.db))
     }
 }
