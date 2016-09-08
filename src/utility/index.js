64b5020a34af621f70dfff84e1a75b7b99c873c6
Francois Best <francoisbest@gmail.com>
2016-09-08 07:57:15 +0200

    Added string hash utility.

--- src/utility/index.js
+++ src/utility/index.js
@@ -3,3 +3,14 @@ export const errorWhile = (context, keyword = 'while') => (error) => {
     console.log(`Error caught ${keyword} ${context}:`, error)
     return Promise.reject(error) // Allow chaining of error handlers
 }
+
+// From http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+export const hash = (str) => {
+    let h = 0
+    for (const i in str) {
+        const c = str.charCodeAt(i)
+        h = ((h << 5) - h) + c
+        h |= 0 // Convert to 32bit integer
+    }
+    return h
+}
