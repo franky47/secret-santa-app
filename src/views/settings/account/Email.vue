0554f3529cc722547f300effd7fbaf6a7b3e9c0d
Francois Best <francoisbest@gmail.com>
2016-09-10 18:53:32 +0200

    Split email/password update sections into own sub-views.

---
+++ src/views/settings/account/Email.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class='ui segments'>
+        <div class='ui secondary segment header'>
+            <h4><i class='ui fitted mail icon'></i>Change your email</h4>
+        </div>
+        <div class='ui segment'>
+            <form class='ui form' :class='{error: error}' @submit.prevent='update'>
+                <email :value.sync='email' v-ref:email></email>
+                <password :value.sync='password' label='Enter your password' placeholder=''></password>
+                <button class='ui button' :class='{loading: loading}' type='submit'>Update email</button>
+            </form>
+        </div>
+        <div class='ui bottom attached error message' v-if='error'>
+            <i class='fitted warning circle icon'></i> {{error}}
+        </div>
+    </div>
+</template>
+
+<script>
+import Email from '../../../components/form/Email'
+import Password from '../../../components/form/Password'
+import { getCurrentUser } from '../../../vuex/modules/user/getters'
+import { updateUserEmail } from '../../../vuex/modules/auth/actions'
+
+export default {
+    data: () => ({
+        email: '',
+        password: '',
+        loading: false,
+        error: null
+    }),
+    components: { Email, Password },
+    methods: {
+        update() {
+            if (!this.email.trim()) {
+                this.error = 'Please enter a valid email address.'
+                return
+            }
+            this.loading = true
+            this.updateUserEmail(this.email.trim(), this.password)
+            .then(() => {
+                this.reset()
+            })
+            .catch(error => {
+                this.loading = false
+                this.error = error
+            })
+        },
+        reset() {
+            this.email      = this.userEmail
+            this.password   = ''
+            this.loading    = false
+            this.error      = null
+        }
+    },
+    watch: {
+        userEmail(value) {
+            this.email = value
+        }
+    },
+    vuex: {
+        getters: {
+            userEmail:  getCurrentUser.email
+        },
+        actions: {
+            updateUserEmail
+        }
+    }
+}
+</script>
---
+++ src/views/settings/account/Password.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class='ui segments'>
+        <div class='ui secondary segment header'><h4><i class='ui fitted lock icon'></i>Change your password</h4></div>
+        <div class='ui segment'>
+            <form class='ui form' :class='{error: error}' @submit.prevent='update'>
+                <password :value.sync='password.current' label='Current password' placeholder=''></password>
+                <password :value.sync='password.updated' label='New password' placeholder=''></password>
+                <password :value.sync='password.confirm' label='Confirm new password' placeholder='' :matching='matching_passwords'></password>
+                <button class='ui button' :class='{loading: loading}' type='submit'>Update password</button>
+            </form>
+        </div>
+        <div class='ui bottom attached error message' v-if='error'>
+            <i class='fitted warning circle icon'></i> {{error}}
+        </div>
+    </div>
+</template>
+
+<script>
+import Password from '../../../components/form/Password'
+import { updateUserPassword } from '../../../vuex/modules/auth/actions'
+
+export default {
+    data: () => ({
+        password: {
+            current: '',
+            updated: '',
+            confirm: ''
+        },
+        loading: false,
+        error: null
+    }),
+    components: { Password },
+    methods: {
+        update() {
+            if (!this.matching_passwords) {
+                this.error = "Passwords don't match."
+                return
+            }
+            this.loading = true
+            this.updateUserPassword(this.password.current, this.password.updated)
+            .then(() => {
+                this.reset()
+            })
+            .catch(error => {
+                this.loading = false
+                this.error  = error
+            })
+        },
+        reset() {
+            this.password.current = ''
+            this.password.updated = ''
+            this.password.confirm = ''
+            this.loading = false
+            this.error = null
+        }
+    },
+    computed: {
+        matching_passwords() {
+            const notEmpty = !!this.password.confirm.trim()
+            const matching = this.password.updated === this.password.confirm
+            return matching && notEmpty
+        }
+    },
+    vuex: {
+        actions: {
+            updateUserPassword
+        }
+    }
+}
+</script>
+
+<style>
+.focus.matching {
+    border-color: green;
+}
+</style>
