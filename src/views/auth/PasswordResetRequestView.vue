27e20247a237bf89e070d499e4e6171139fd40c3
Francois Best <francoisbest@gmail.com>
2016-09-10 20:45:49 +0200

    Password reset request UI wip.

--- src/views/auth/PasswordResetRequestView.vue
+++ src/views/auth/PasswordResetRequestView.vue
@@ -1,11 +1,26 @@
 <template>
-    <p>Enter your email to reset your password</p>
+    <div class='ui two column doubling grid'>
+        <div class='ui centered column'>
+            <div class='ui padded raised segment'>
+                <h3>Reset your password</h3>
+                <form class='ui form'>
+                    <email></email>
+                    <button class='ui fluid primary button'>
+                        <i class='fitted mail icon'></i>Send password reset email
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Email from '../../components/form/Email'
+
 export default {
     data: () => ({
 
-    })
+    }),
+    components: { Email }
 }
 </script>
