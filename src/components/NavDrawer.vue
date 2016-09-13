c811eee088e3ff28950c11a6195600796ed0f8dc
Francois Best <francoisbest@gmail.com>
2016-09-13 15:23:16 +0200

    Bound nav drawer to signed in user info.

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,18 +1,23 @@
 <template>
     <div class='ui sidebar vertical menu overlay' :class='{visible: open}'
         v-on-clickaway='dismiss'>
-        <div class='item'>
-            <img class='ui avatar image' src='http://placehold.it/100'>
-            <span class='header'>Foo Bar</span>
+        <div class='item' v-if='isSignedIn'>
+            <img class='ui avatar image' :src='userPhoto'>
+            <span class='header'>{{userName}}</span>
         </div>
-        <div class='item'>
+        <div class='item' v-if='!isSignedIn'>
+            <a @click.stop='signIn'>{{ $t('auth.signIn') }}</a>
+        </div>
+        <div class='item' v-if='isSignedIn'>
             <i class='settings icon'></i>Settings
             <div class='menu'>
                 <a class='item' v-link='{ path: routes.settings.account }'>Account</a>
                 <a class='item' v-link='{ path: routes.settings.profile }'>Profile</a>
             </div>
         </div>
-        <a class='item' @click.stop='signOut'><i class='icon sign out'></i>{{ $t('auth.signOut') }}</a>
+        <a class='item' @click.stop='signOut' v-if='isSignedIn'>
+            <i class='icon sign out'></i>{{ $t('auth.signOut') }}
+        </a>
     </div>
 </template>
 
@@ -20,7 +25,8 @@
 import { mixin as clickaway } from 'vue-clickaway'
 import * as routes      from '../router/routes-definitions'
 import * as ui          from '../vuex/modules/ui'
-import { signOut }      from '../vuex/modules/auth/actions'
+import * as auth        from '../vuex/modules/auth'
+import { getCurrentUser } from '../vuex/modules/user/getters'
 
 export default {
     data: () => ({
@@ -32,15 +38,22 @@ export default {
             if (this.open) {
                 this.close()
             }
+        },
+        signIn() {
+            this.dismiss()
+            this.$router.go(routes.auth.signIn)
         }
     },
     vuex: {
         getters: {
-            open:   ui.getters.isNavDrawerOpen
+            open:       ui.getters.isNavDrawerOpen,
+            userName:   getCurrentUser.displayName,
+            userPhoto:  getCurrentUser.photoURL,
+            isSignedIn: auth.getters.isSignedIn
         },
         actions: {
-            close: ui.actions.closeNavDrawer,
-            signOut
+            close:      ui.actions.closeNavDrawer,
+            signOut:    auth.actions.signOut
         }
     }
 }
