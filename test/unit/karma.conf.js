0a3a76dbd0a9d17814f4684f9de449db6c978a83
Francois Best <francoisbest@gmail.com>
2016-09-16 18:43:36 +0200

    Swapped isparta with istanbul for coverage.

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,65 +11,65 @@ const webpack = require('webpack')
 const projectRoot = path.resolve(__dirname, '../../')
 
 const webpackConfig = merge(baseConfig, {
-  // use inline sourcemap for karma-sourcemap-loader
-  module: {
-    loaders: utils.styleLoaders()
-  },
-  devtool: '#inline-source-map',
-  vue: {
-    loaders: {
-      js: 'isparta'
-    }
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': require('../../config/env.test')
-    })
-  ]
+    // use inline sourcemap for karma-sourcemap-loader
+    module: {
+        loaders: utils.styleLoaders()
+    },
+    devtool: '#inline-source-map',
+    vue: {
+        loaders: {
+            js: 'istanbul-instrumenter!babel'
+        }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': require('../../config/env.test')
+        })
+    ]
 })
 
 // no need for app entry during tests
 delete webpackConfig.entry
 
-// make sure isparta loader is applied before eslint
+// make sure istanbul-instrumenter loader is applied before eslint
 webpackConfig.module.preLoaders = webpackConfig.module.preLoaders || []
 webpackConfig.module.preLoaders.unshift({
-  test: /\.js$/,
-  loader: 'isparta',
-  include: path.resolve(projectRoot, 'src')
+    test: /\.js$/,
+    loader: 'istanbul-instrumenter!babel',
+    include: path.resolve(projectRoot, 'src')
 })
 
-// only apply babel for test files when using isparta
+// only apply babel for test files when using istanbul-instrumenter
 webpackConfig.module.loaders.some(function (loader, i) {
-  if (loader.loader === 'babel') {
-    loader.include = path.resolve(projectRoot, 'test/unit')
-    return true
-  }
+    if (loader.loader === 'babel') {
+        loader.include = path.resolve(projectRoot, 'test/unit')
+        return true
+    }
 })
 
-module.exports = function (config) {
-  config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai'],
-    reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
-    preprocessors: {
-      './index.js': ['webpack', 'sourcemap']
-    },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      noInfo: true
-    },
-    coverageReporter: {
-      dir: './coverage',
-      reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
-      ]
-    }
-  })
+module.exports = function(config) {
+    config.set({
+        // to run in additional browsers:
+        // 1. install corresponding karma launcher
+        //    http://karma-runner.github.io/0.13/config/browsers.html
+        // 2. add it to the `browsers` array below.
+        browsers: ['PhantomJS'],
+        frameworks: ['mocha', 'sinon-chai'],
+        reporters: ['spec', 'coverage'],
+        files: ['./index.js'],
+        preprocessors: {
+            './index.js': ['webpack', 'sourcemap']
+        },
+        webpack: webpackConfig,
+        webpackMiddleware: {
+            noInfo: true
+        },
+        coverageReporter: {
+            dir: './coverage',
+            reporters: [
+                { type: 'lcov', subdir: '.' },
+                { type: 'text-summary' }
+            ]
+        }
+    })
 }
