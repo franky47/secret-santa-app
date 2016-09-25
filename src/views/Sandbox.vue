34aca4393d5ea0c94031cae07de8cbe03011a03f
Francois Best <francoisbest@gmail.com>
2016-09-25 09:25:21 +0200

    Sandboxing clickaway on iOS

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,49 +1,18 @@
 <template>
     <div>
-        <input type='range' v-model='size' min='50' max='500'>
-        <button @click='changeColor'>Change color</button>
-        <button @click='render'>Render</button>
-        <br>
-        <generic-avatar :size='size' text='X' v-ref:avatar></generic-avatar>
-        <img :src='url' :width='size' :height='size'>
-        <br>
-
+        <div v-on-clickaway='text="You clicked away"' @click='text="You clicked me"'>
+            <p>{{text}}</p>
+        </div>
     </div>
 </template>
 
 <script>
-import GenericAvatar from '../components/GenericAvatar'
-import { avatarColors } from '../utility/material-colors'
+import { mixin as clickaway } from 'vue-clickaway'
 
 export default {
     data: () => ({
-        size: 100,
-        url: null
+        text: 'Click somewhere'
     }),
-    components: {
-        GenericAvatar
-    },
-    methods: {
-        changeColor() {
-            const pick = array => array[Math.floor(Math.random() * array.length)]
-            const color = pick(avatarColors)
-            this.$refs.avatar.backColor = color
-        },
-        render() {
-            this.$refs.avatar.getAsFile().then(file => {
-                this.readFromFile(file)
-            })
-        },
-        readFromFile(file) {
-            const reader = new window.FileReader()
-            reader.onload = (event) => {
-                this.readFromURL(event.target.result)
-            }
-            reader.readAsDataURL(file)
-        },
-        readFromURL(url) {
-            this.url = url
-        }
-    }
+    mixins: [ clickaway ]
 }
 </script>
