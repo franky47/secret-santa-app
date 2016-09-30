5b4b2e43d7e7672efcc7bdce09f5ff66f9762be2
Francois Best <francoisbest@gmail.com>
2016-09-30 07:31:03 -0700

    Cleanup.

--- builder/build.js
+++ builder/build.js
@@ -1,8 +1,6 @@
 // https://github.com/shelljs/shelljs
 require('shelljs/global')
 env.NODE_ENV = 'production'
-
-const chalk = require('chalk')
 const path = require('path')
 const config = require('../config')
 const ora = require('ora')
@@ -22,12 +20,9 @@ const spinner = new ora({
 spinner.start()
 
 const assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
-console.log(chalk.blue('Assets path: '), assetsPath)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
-console.log('Pre-build status:')
-console.log(ls('-lA', assetsPath))
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
@@ -39,6 +34,4 @@ webpack(webpackConfig, function (err, stats) {
     chunks: false,
     chunkModules: false
   }) + '\n')
-  console.log('Post-build status:')
-  console.log(ls('-lA', assetsPath))
 })
